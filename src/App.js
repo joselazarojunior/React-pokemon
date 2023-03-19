ca24ca2ec1939449942cab8df91721bff1a677ed
joselazarojunior <j.lazaro.junior@gmail.com>
2023-03-19 18:19:48 -0300

    Projeto ReactJS consuimindo API de Pokemons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+
+
+const fetchPokemon = () => {
+  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+  const pokemonPromises = [];
+
+  for(let i=1; i <= 150; i++){
+    pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()));
+     
+  }
+
+  Promise.all(pokemonPromises)
+    .then(pokemons => {
+      
+      const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+
+        accumulator += `
+        <div class="card">
+          <img class="card-img ${types[0]}" alt="${pokemon.name}" src="${pokemon.sprites.front_default}" />
+          <h2 class="card-title">${pokemon.id}.  ${pokemon.name}</h2>
+          <p class="card-subtitle">${types.join('  |  ')}</p>
+        </div>`
+        return accumulator
+
+      }, '')
+
+      const div = document.querySelector('.poke-container');
+      div.innerHTML = lisPokemons;
+    }) 
+}
+
+fetchPokemon();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='poke-container'>
+      {/* <div className='container'>
+        
+      </div> */}
+
     </div>
+    
   );
 }
+
+
 
 export default App;
